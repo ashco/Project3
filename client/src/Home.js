ce561fc498c5ce9177bf20e990b215807ea9fd49
ashco <ashchristie@gmail.com>
2018-01-29 11:26:42 -0800

    init cleanup

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Home extends Component {
   render(){
     return (
-        <p>This is a home page. You probably ought to write something here</p>
-      );
+      <div>
+        Thing
+      </div>
+    );
   }
 }
 
--- client/src/layout/Nav.js
+++ client/src/layout/Nav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import Logout from '../auth/Logout.js';
 import { Link } from 'react-router-dom';
 
@@ -27,10 +26,6 @@ class Nav extends Component {
             <a href="/">Home</a>
             {links}
           </nav>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to MERN Stack Boilerplate!</h1>
-          </header>
         </div>
       );
   }
