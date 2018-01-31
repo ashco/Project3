bdfdf21ad36f9a61137016583a33e9c504811640
ashco <ashchristie@gmail.com>
2018-01-31 10:33:21 -0800

    starting materialize integration

--- client/src/main/Home.js
+++ client/src/main/Home.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import Title from '../layout/Title.js';
+import { Link } from 'react-router-dom';
+// MATERIAL UI
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 class Home extends Component {
   render(){
+
+    const style = {
+      margin: 12,
+    };    
+
     return (
         <div className="home">
 
           <Title text="Dream Home Page" style="Home__title" />
 
           <div className="login-box">
-            <a href="#" className="home__btn--signup box">Signup</a>
-            <a href="#" className="home__btn--login box">Login</a>
+            
+            <Link to="/login"><RaisedButton label="Login" primary={true} style={style} /></Link>
+            <Link to="/signup"><RaisedButton label="Sign Up" primary={true} style={style} /></Link>
           </div>
         </div>
     );
