ef2ff9d4f59f46b03afd7389f1155fe8291613f8
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-31 17:55:51 -0800

    updated formatting on dreamscore page, added sort functionality for data, added color palette

--- client/src/scripts/dataCleansing.js
+++ client/src/scripts/dataCleansing.js
@@ -1,16 +1,25 @@
 export function doughnutChartData(dataSet){
 
+	// final array for formatted data
 	var formattedData = [];
 
+	// reformat object into array of objects with key value pairs
 	for(var key in dataSet){
 	  if(key != 'sentiment'){
+	  	var tmp = key.split("_")
+
 	    var child = {
-	      name: (key.split("_")).join(' '),
+	      name: tmp[1],
 	      value: Number(dataSet[key])
 	    }
 	    formattedData.push(child);
 	  }
 	}
+
+	// sort data by sentiment name
+	formattedData.sort(function(a, b) {
+		return b.name - a.name ;
+	});	
 
 	return formattedData;
 }
