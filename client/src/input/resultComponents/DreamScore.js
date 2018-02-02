1b083c2af4a0d0edbbefe77b51c5209dcb051473
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-02-02 12:12:59 -0800

    updated charts with tooltips and data

--- client/src/input/resultComponents/DreamScore.js
+++ client/src/input/resultComponents/DreamScore.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { ResponsiveContainer, PieChart, Pie, Sector, Cell,  Tooltip } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Sector, Cell, Legend, Tooltip } from 'recharts';
 import {doughnutChartData} from '../../scripts/dataCleansing.js'
-import './PieChart.css';
 
 
 class DreamScore extends Component {
@@ -16,24 +15,33 @@ class DreamScore extends Component {
 	const COLORS = ['#A1D4E3', '#F98285', '#BD70B3', '#E9CC84'];
 
 	const RADIAN = Math.PI / 180; 
-// function to add percent labels if wanted
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
- 
-    return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'}  dominantBaseline="central">
-          {`${(percent * 100).toFixed(0)}%`}
-        </text>
+  
+  // function to add percent labels if wanted
+  const renderTooltipContent = (props) => {
+      const { active, payload } = props;
+
+    if (active && payload && payload.length) {
+      const data = payload[0].payload;
+      const percent = Math.round(data.value*100)
+      return (
+        <div className="customizedToolTip">
+          <p className="ToolTipTitle">{data.name}: {percent} %</p>
+        </div>
       );
-    };
+    }
+
+    return null;
+  }
+
+
 
   	return (
   		<div className="DreamScore__box box">
       <h1> Overall Sentiment: {rawData.sentiment} </h1>
-  		<PieChart width={400} height={400} onMouseEnter={this.onPieEnter}>
-        
+      <ResponsiveContainer height={440}>
+  		<PieChart onMouseEnter={this.onPieEnter}>
+      <Tooltip content={renderTooltipContent}/>
+        <Legend verticalAlign="top" height={36}/>
         <Pie
           data={data} 
           cx={200} 
@@ -49,6 +57,7 @@ class DreamScore extends Component {
           }
         </Pie>
       </PieChart>
+      </ResponsiveContainer>
   		</div>
     );
   }
