c119b69544ab69f119ae3c0de34a336e14a9aa16
ashco <ashchristie@gmail.com>
2018-02-03 16:24:11 -0800

    messing with aws

--- routes/helpers/textAnalysis.js
+++ routes/helpers/textAnalysis.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 var AWS = require('aws-sdk');
 // AWS.config.loadFromPath('./awsconfig.json');
 AWS.config.update({region: 'us-west-2'});
-new AWS.Credentials(process.env.AWS_KEY_ID, process.env.AWS_SECRET_KEY);
+// new AWS.Credentials(process.env.AWS_KEY_ID, process.env.AWS_SECRET_KEY);
+new AWS.Credentials('AKIAINT7TT6PJ2ZS43NQ', 'eIfz/5tFO85flQRq2BKtoQhBIxH22wNrrAismyMo');
 
 
 var comprehend = new AWS.Comprehend();
