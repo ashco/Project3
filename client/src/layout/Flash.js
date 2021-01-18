e496d085d070aa6c458efb2aa0ec3a10014469e4
Ashton <10988492+ashco@users.noreply.github.com>
2021-01-18 12:06:41 -0800

    Set up a quick fix for alert

--- client/src/layout/Flash.js
+++ client/src/layout/Flash.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Flash extends Component {
   cancelMessage = () => {
     this.props.cancelFlash();
-  }
+  };
 
   render() {
-    console.log('props inside of flash component', this.props)
-    let flashType = 'error';
-    if(this.props && this.props.flashType){
+    console.log("props inside of flash component", this.props);
+    let flashType = "error";
+    if (this.props && this.props.flashType) {
       flashType = this.props.flashType;
     }
-    if(this.props && this.props.flash){
+    if (this.props && this.props.flash) {
       return (
-        <div className={flashType}>
-          {this.props.flash} <button className="x-button" onClick={this.cancelMessage}>X</button>
-        </div>);
-    }
-    else {
-      return (<div className="no-flash">No Flash</div>);
+        <div className={`flash ${flashType}`}>
+          <p style={{ marginLeft: "30px" }}>{this.props.flash}</p>
+          <button
+            style={{ marginRight: "30px" }}
+            className="x-button"
+            onClick={this.cancelMessage}
+          >
+            X
+          </button>
+        </div>
+      );
+    } else {
+      return <div className="no-flash">No Flash</div>;
     }
   }
 }
